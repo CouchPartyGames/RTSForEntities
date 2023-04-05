a2f3533c49a8f9196bdc33823270a0b8e3b98811
CouchPartyGames <83233322+CouchPartyGames@users.noreply.github.com>
2023-04-05 17:28:17 -0400

    chore: add new inputs, use forward+ for URP rendering path

---
+++ Assets/Scripts/Systems/PlayerInputSystem.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using UnityEngine.InputSystem;
+
+namespace RTSForEntities.Systems {
+    public partial class PlayerInputSystem : SystemBase {
+        protected override void OnUpdate() {
+            
+        }
+    }
+}
