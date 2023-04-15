f775655ac8d3aa8f0952bdaa8f11bed362293add
CouchPartyGames <83233322+CouchPartyGames@users.noreply.github.com>
2023-04-15 15:45:24 -0400

    chore: add documentation for project

--- Assets/Scripts/Editor/Documentation.cs
+++ Assets/Scripts/Editor/Documentation.cs
@@ -5,17 +5,17 @@ namespace Editor {
     public class Documentation : MonoBehaviour {
         [MenuItem("RTS Entities/Github", false, 51)]
         public static void Github() {
-            
+            Application.OpenURL("https://github.com/CouchPartyGames/RTSForEntities");
         }
 
         [MenuItem("RTS Entities/YouTube", false, 52)]
         public static void Youtube() {
-            
+            Application.OpenURL("https://www.youtube.com/@couchpartygames/playlists");
         }
 
         [MenuItem("RTS Entities/Documentation", false, 53)]
         public static void Docu() {
-            
+            Application.OpenURL("");
         }
     }
 }
